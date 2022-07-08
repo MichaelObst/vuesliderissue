13a5ead2c09b1cfe4203c381ba8cc2abd1370ac4
MichaelObst <93293813+MichaelObst@users.noreply.github.com>
2022-07-08 10:51:07 +1000

    Initial commit

---
+++ App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    {{ cValue }}
+    <p>CVALUE ALWAYS PREVIOUS VALUE OF SLIDER</p>
+    <HelloWorld v-model="testValue" @change="doSomethingWithVal()" />
+  </div>
+</template>
+
+<script>
+import HelloWorldVue from "./components/HelloWorld.vue";
+export default {
+  name: "App",
+  components: {
+    HelloWorld: HelloWorldVue,
+  },
+
+  data: function () {
+    return {
+      testValue: 10,
+      cValue: 0,
+    };
+  },
+
+  methods: {
+    doSomethingWithVal: function () {
+      this.cValue = this.testValue;
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    {{ cValue }}
+    <p>Can't return to zero</p>
+    <vue-slider @change="doSomethingWithVal" :min="-1" :max="4" />
+  </div>
+</template>
+
+<script>
+import VueSlider from "vue-slider-component";
+import "vue-slider-component/theme/antd.css";
+export default {
+  name: "App",
+  components: {
+    VueSlider,
+  },
+
+  data: function () {
+    return {
+      testValue: 2,
+      cValue: 0,
+    };
+  },
+
+  methods: {
+    doSomethingWithVal: function (value) {
+      this.cValue = value;
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
